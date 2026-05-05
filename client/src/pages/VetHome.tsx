531fcc8e90db485379f27cc018fe10ef2e0bcff4
Danyel Gripp <danyel.gripp@gmail.com>
2026-05-05 17:30:48 -0300

    fix: GuaranteeSection restaurada como seção destacada após PricingSection

--- client/src/pages/VetHome.tsx
+++ client/src/pages/VetHome.tsx
@@ -8,6 +8,7 @@ import MethodologySection from "@/components/MethodologySection";
 import ResultsSection from "@/components/ResultsSection";
 import ComparisonSection from "@/components/ComparisonSection";
 import PricingSection from "@/components/PricingSection";
+import GuaranteeSection from "@/components/GuaranteeSection";
 import QuemESection from "@/components/QuemESection";
 import EscassezSection from "@/components/EscassezSection";
 import FAQSection from "@/components/FAQSection";
@@ -27,6 +28,7 @@ export default function VetHome() {
       <ResultsSection />
       <ComparisonSection />
       <PricingSection />
+      <GuaranteeSection />
       <QuemESection />
       <EscassezSection />
       <FAQSection />
